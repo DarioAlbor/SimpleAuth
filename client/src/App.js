c876c15efe63a8d678b8a0ab4c3ea720fabfd8d6
DarioAlbor <darioalbor03@gmail.com>
2024-07-04 17:12:30 -0300

    new button colormode

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
+import ColorMode from './components/colormode';
 
 function App() {
   return (
-    <Router>
-      <Routes />
-    </Router>
+    <div>
+      <Router>
+        <Routes />
+      </Router>
+      <div style={{ position: 'fixed', top: '-45%', left: '1%', zIndex: 1000 }}>
+        <ColorMode />
+      </div>
+    </div>
   );
 }
 
